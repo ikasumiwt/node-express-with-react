afc423773d97981850b15c58b6d3dfedbadca94e
ikasumi_wt <wtsnyk13@gmail.com>
2015-11-11 12:40:53 +0900

    test

--- app.js
+++ app.js
@@ -27,16 +27,16 @@ app.use( cookieParser() );
 app.use( express.static( path.join( __dirname, 'public') ) );
 
 app.use( '/', routes );
-app.use( '/lists', lists );
-app.use( '/test', test );
+//app.use( '/lists', lists );
+//app.use( '/test', test );
 
 // catch 404 and forward to error handler
 app.use( function( req, res, next ) {
 
-        var err = new Error( 'Not Found' );
-        err.status = 404;
-        next( err );
-        });
+    var err = new Error( 'Not Found' );
+    err.status = 404;
+    next( err );
+});
 
 // error handlers
 
@@ -44,24 +44,24 @@ app.use( function( req, res, next ) {
 // will print stacktrace
 if (app.get( 'env' ) === 'development' ) {
     app.use( function( err, req, res, next ) {
-            res.status( err.status || 500 );
-            res.render( 'error', {
-message: err.message,
-error: err
-} );
-            } );
+        res.status( err.status || 500 );
+        res.render( 'error', {
+            message: err.message,
+            error: err
+        } );
+    } );
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use( function( err, req, res, next ) {
 
-        res.status( err.status || 500 );
-        res.render( 'error', {
-message: err.message,
-error: {}
+    res.status( err.status || 500 );
+    res.render( 'error', {
+        message: err.message,
+        error: {}
+    } );
 } );
-        } );
 
 
 module.exports = app;
