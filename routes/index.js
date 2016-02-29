eb53fd24243225a61b050edb1f403ba35e3cd1b9
ikasumi_wt <wtsnyk13@gmail.com>
2016-02-29 21:22:39 +0900

    test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get( '/', function( req, res ) {
-    res.render( 'index', { title: 'test' } );
+    res.render( 'index', { title: 'testaaaaa' } );
 } );
 
 module.exports = router;
